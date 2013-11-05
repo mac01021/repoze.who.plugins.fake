472dae869b64429e407c5fb53e058ed4d57f7a36
Matthew Coolbeth <coolbeth@gmail.com>
2013-11-05 15:57:18 -0500

    added the script-name authenticator and some documentation

--- README.md
+++ README.md
@@ -2,3 +2,25 @@ repoze.who.plugins.fake
 =======================
 
 Fake authenticators that may be useful for testing your web application
+
+
+
+To use, add the following entries to your who.ini file
+
+    [plugin:fakeauth]
+    use = repoze.who.plugins.fake:*{plugin-class-of-your-choosing}*
+
+    [authenticators]
+    plugins = fakeauth
+
+
+Currently available plugin classes include
+
+-   AllAcceptingAuthenticationPlugin - allows you to log in as any
+    username without worrying about the password.  Use with the basicauth
+    challenger
+
+-   ScriptNameAuthenticationPlugin - Never challenges the user.  Uses the
+    SCRIPT_NAME from the application environment as the login name.
+
+
--- repoze/who/plugins/fake/__init__.py
+++ repoze/who/plugins/fake/__init__.py
@@ -1,9 +1,13 @@
 
 class AllAcceptingAuthenticationPlugin(object):
- 
+    """ When challenged, the client can specify whatever
+    user name she likes, and this authenticator will
+    believe her.
+    """
+
     def __init__(self, *args, **kwargs):
-    pass
- 
+        pass
+
     # IAuthenticatorPlugin
     def authenticate(self, environ, identity):
         try:
@@ -13,3 +17,30 @@ class AllAcceptingAuthenticationPlugin(object):
         except KeyError:
             pass
         return None
+
+
+
+class ScriptNameAuthenticationPlugin(object):
+    """ Never challenges the client.  Authenticates
+    as the name of the script that the client has
+    requested.
+    """
+ 
+    #IAuthenticatorPlugin
+    def authenticate(self, environ, identity):
+        return identity['login']
+ 
+    #IIdentifierPlugin
+    def identify(self, environ):
+        return {'login': environ['SCRIPT_NAME'][1:],
+                'password': None}
+ 
+    #IIdentifierPlugin
+    def remember(self, environ, identity):
+        pass
+ 
+    #IIdentifierPlugin
+    def forget(self, environ, identity):
+        pass
+
+
